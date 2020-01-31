55b0f0117b202076cadf4a3031df442fbc803dec
Gerard Crockenberg <gmahred@gmail.com>
2020-01-31 13:04:07 -0500

    finished project and stretch

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,40 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { makeStyles } from '@material-ui/styles';
+import { Container } from '@material-ui/core';
 
 import Login from "./components/Login";
+import BubblePage from "./components/BubblePage";
+import PrivateRoute from "./components/PrivateRoute";
 import "./styles.scss";
 
+const useStyles = makeStyles({
+  Container: {
+    background: 'white',
+    justifyContent: 'right',
+    alignItems: 'center',
+    borderRadius: 2,
+    padding: 20,
+  },
+})
+
 function App() {
+  const classes = useStyles();
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+        <Switch>
+          <PrivateRoute path="/protected" component={BubblePage} />
+          <Container className={classes.Container}>
+            <Route path="/login" component={Login} />
+            <Route exact path="/" component={Login} />
+            <Route component={Login} />
+          </Container>
+        </Switch>
       </div>
     </Router>
   );
